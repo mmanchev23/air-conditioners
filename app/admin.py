cd062936ef921615e4f2f2a7da9c6ccd33f3a378
mmanchev23 <mmanchev2003@gmail.com>
2022-05-01 10:33:04 +0300

    Initial commit ...

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,9 @@
 from .models import *
 from django.contrib import admin
 
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ["title", "technician"]
+
+
 admin.site.register(User)
+admin.site.register(Application, ApplicationAdmin)
--- app/models.py
+++ app/models.py
@@ -13,3 +13,23 @@ class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=13, choices=ROLES, default="Customer")
     pass
+
+
+class Application(models.Model):
+    STATUS = (
+        ("Waiting ...", "Waiting ..."),
+        ("Expecting a visit!", "Expecting a visit!"),
+        ("In progress ...", "In progress ..."),
+        ("Completed", "Completed"),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=30, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    address = models.CharField(max_length=30, null=False, blank=False)
+    image = models.ImageField(default="application.jpg", null=False, blank=False)
+    status = models.CharField(max_length=18, choices=STATUS, default="Waiting ...")
+    date_of_visit_by_technician = models.DateField(null=False, blank=False)
+    technician = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True, editable=False)
